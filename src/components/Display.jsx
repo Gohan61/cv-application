3c3ad37cc5fffa52ee780ba896807d5551bf6e42
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-23 16:09:57 +0200

    Feat: display educational experience on submit

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -7,6 +7,12 @@ export default function Display({ input }) {
         <p>Email: {input.email}</p>
         <p>Phone number: {input.number}</p>
       </section>
+      <section>
+        <h2>Educational experience</h2>
+        <p>Educational experience: {input.school}</p>
+        <p>Title of degree: {input.study}</p>
+        <p>Year of graduation: {input.graduation}</p>
+      </section>
     </>
   );
 }
