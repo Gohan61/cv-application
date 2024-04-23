04254fe1bb4ada9edcbfe993bedae1fff715a697
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-04-23 16:49:52 +0200

    Feat: input & display practical experience

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -13,6 +13,21 @@ export default function Display({ input }) {
         <p>Title of degree: {input.study}</p>
         <p>Year of graduation: {input.graduation}</p>
       </section>
+      <section>
+        <h2>Practial experience</h2>
+        <p>Company name: {input.company}</p>
+        <p>Job position: {input.position}</p>
+        <p>Main responsibilites:</p>
+        <ul>
+          <li>{input.responsibilities1}</li>
+          <li>{input.responsibilities2}</li>
+          <li>{input.responsibilities3}</li>
+        </ul>
+        <div>
+          <p>Start of job: {input.startDate}</p>
+          <p>End of job: {input.endDate}</p>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,6 +22,27 @@ export default function Form({ changeShow, input, saveInput }) {
       case e.target.id === "graduation":
         saveInput({ ...input, graduation: e.target.value });
         break;
+      case e.target.id === "company":
+        saveInput({ ...input, company: e.target.value });
+        break;
+      case e.target.id === "position":
+        saveInput({ ...input, position: e.target.value });
+        break;
+      case e.target.id === "responsibilities1":
+        saveInput({ ...input, responsibilities1: e.target.value });
+        break;
+      case e.target.id === "responsibilities2":
+        saveInput({ ...input, responsibilities2: e.target.value });
+        break;
+      case e.target.id === "responsibilities3":
+        saveInput({ ...input, responsibilities3: e.target.value });
+        break;
+      case e.target.id === "startDate":
+        saveInput({ ...input, startDate: e.target.value });
+        break;
+      case e.target.id === "dateEnd":
+        saveInput({ ...input, endDate: e.target.value });
+        break;
     }
   }
 
@@ -81,6 +102,70 @@ export default function Form({ changeShow, input, saveInput }) {
             id="graduation"
             name="graduation"
             value={input.graduation}
+            onChange={submitInfo}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Practical experience</legend>
+          <label htmlFor="company">Company name: </label>
+          <input
+            type="text"
+            id="company"
+            name="company"
+            value={input.company}
+            onChange={submitInfo}
+          />
+          <label htmlFor="position">Job position: </label>
+          <input
+            type="text"
+            id="position"
+            name="position"
+            value={input.position}
+            onChange={submitInfo}
+          />
+          <ul name="responsibilities">
+            <label htmlFor="responsibilites">
+              Main responsibilites in your jobs:{" "}
+            </label>
+            <li>
+              <input
+                type="text"
+                value={input.responsibilities1}
+                onChange={submitInfo}
+                id="responsibilities1"
+              />
+            </li>
+            <li>
+              <input
+                type="text"
+                value={input.responsibilities2}
+                onChange={submitInfo}
+                id="responsibilities2"
+              />
+            </li>
+            <li>
+              <input
+                type="text"
+                value={input.responsibilities3}
+                onChange={submitInfo}
+                id="responsibilities3"
+              />
+            </li>
+          </ul>
+          <label htmlFor="dateStart">Start of job: </label>
+          <input
+            type="date"
+            id="startDate"
+            name="startDate"
+            value={input.startDate}
+            onChange={submitInfo}
+          />
+          <label htmlFor="dateEnd">End of job: </label>
+          <input
+            type="date"
+            name="dateEnd"
+            id="dateEnd"
+            value={input.endDate}
             onChange={submitInfo}
           />
         </fieldset>
