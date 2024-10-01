44ab404aac280484f62321001733e74c60dac1ee
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-10-01 16:57:03 +0200

    Chore: define types for state of components

---
+++ src/types/formTypes.ts
@@ -0,0 +1,20 @@
+export interface personalInfoType {
+  name: string | undefined;
+  email: string | undefined;
+  phoneNumber: number | undefined;
+}
+
+export interface educationType {
+  school: string | undefined;
+  study: string | undefined;
+  graduation: string | undefined;
+}
+
+export interface practicalType {
+  company: string | undefined;
+  position: string | undefined;
+  startDate: string | undefined;
+  endDate: string | undefined;
+}
+
+export type responsibilityType = string | undefined;
